234e3cf159816d941d039a25986d7bcc2c0fcc94
POPOVICI C.T. GEORGE-OCTAVIAN <popovicigeorge3@gmail.com>
2024-02-22 00:30:52 +0200

    Create linked_list_cycle.java

---
+++ Leetcode_Java_problems/linked_list_cycle.java
@@ -0,0 +1,21 @@
+class ListNode {
+     int val;
+     ListNode next;
+     ListNode(int x) {
+         val = x;
+         next = null;
+     }
+  }
+  class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode fast = head;
+        while(fast!=null && fast.next!=null)
+        {
+            head = head.next;
+            fast = fast.next.next;
+            if(head==fast)
+                return true;
+        }
+        return false;
+    }
+}
